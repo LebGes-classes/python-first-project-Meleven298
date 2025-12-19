ce29cc281a9c16d055dcec11d69f0aec70554a85
Meleven298 <valeevmarsel2007@gmail.com>
2025-12-19 23:50:21 +0300

    Create WhilePlayingMenu.py

---
+++ WhilePlayingMenu.py
@@ -0,0 +1,10 @@
+class WhilePlayingMenu:
+    def init(self,player, labyrinth):
+        self.player = player
+        self.labyrinth = labyrinth
+        
+    def stop_game(self):
+        pass
+    
+    def counter(self):
+        pass
