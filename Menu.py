52b988eb1ecf71a03792ec582198fa29dc205744
Meleven298 <valeevmarsel2007@gmail.com>
2025-12-19 23:50:19 +0300

    Create Menu.py

---
+++ Menu.py
@@ -0,0 +1,18 @@
+class Menu:
+    def init(self, player, labyrinth):
+        self.player = player
+        self.labyrinth = labyrinth
+        
+    def display_menu(self):
+        print()
+        print()
+        print()
+    
+    def next_level(self):
+        pass
+    
+    def start_game(self):
+        pass
+    
+    def choose_dificulty(self):
+        pass
